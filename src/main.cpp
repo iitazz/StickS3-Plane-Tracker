8099ed99d5de515f9970c7aa9ff55704274ae3ad
taku <taku>
2026-09-05 23:38:02 +0200

    Bump version to 1.0.25

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ bool controlShowIp() { return M5.BtnA.wasHold(); }
 constexpr char ROUTE_URL_PREFIX[] = "https://api.adsbdb.com/v0/callsign/";
 constexpr char SETUP_AP_NAME[] = "PlaneTracker-Setup";
 constexpr char SETUP_AP_PASSWORD[] = "planeconfig";
-constexpr char FIRMWARE_VERSION[] = "1.0.24";
+constexpr char FIRMWARE_VERSION[] = "1.0.25";
 
 constexpr char DEBUG_PAGE[] = R"rawliteral(
 <!doctype html><html><head><meta name="viewport" content="width=device-width,initial-scale=1">
